b031d489d0df2fb311248f9b9dddd62648ac8d70
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-09 16:21:08 -0500

    added set command

--- Cart.sh
+++ Cart.sh
@@ -3,12 +3,12 @@ set -e
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 
-
 useradd roboshop
 
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
 cd /home/roboshop
-unzip /tmp/cart.zip
+rm -rf cart
+unzip -o /tmp/cart.zip
 mv cart-main cart
 cd cart
 npm install
