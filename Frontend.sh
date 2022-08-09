0b3673a8cb6cdbdcd167dc5ee376a6a90321e963
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-09 10:06:48 -0500

    added manual step in the sh file

--- Frontend.sh
+++ Frontend.sh
@@ -1,1 +1,15 @@
 # The frontend is the service in RobotShop to serve the web content over Nginx.
+
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/static/* .
+# mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx
