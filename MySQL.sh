a0eb10d72589aee0a8de47ca9bdbb1fb7dc34219
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-10 00:53:13 -0500

    updating ./RoboShop-Automation

--- MySQL.sh
+++ MySQL.sh
@@ -1,15 +1,15 @@
 set -e
 echo "setting nodejs repos"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>/tmp/mysql.log
 echo "Installing mysql "
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>/tmp/mysql.log
 
 systemctl enable mysqld
 systemctl start mysqld
 
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-echo "alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
+echo "alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';"  | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
 
 mysql -uroot -pRoboShop@1
 
@@ -17,7 +17,7 @@ mysql -uroot -pRoboShop@1
 
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>/tmp/mysql.log
 
-cd /tmp &>>/tmp/mysql.log
-unzip mysql.zip &>>/tmp/mysql.log
-cd mysql-main &>>/tmp/mysql.log
-mysql -u root -pRoboShop@1 <shipping.sql &>>/tmp/mysql.log
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -u root -pRoboShop@1 <shipping.sql
