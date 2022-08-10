1f0ab3523dde386446e6e4e08f68cd71e40cfca7
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-10 15:20:21 -0500

    updating ./RoboShop-Automation

--- Cart.sh
+++ Cart.sh
@@ -9,37 +9,37 @@ status_check()
 
 echo "setting nodejs repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
-status_check()
+status_check();
 
 echo "Installing NodeJS"
 yum install nodejs -y &>>/tmp/cart.log
-status_check()
+status_check();
 
 echo "Adding user"
 useradd roboshop &>>/tmp/cart.log
-status_check()
+status_check();
 
 echo "Downloading application content"
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" && cd /home/roboshop &>>/tmp/cart.log
-status_check()
+status_check();
 
 echo "Deleting the old application content"
 rm -rf cart &>>/tmp/cart.log
-status_check()
+status_check();
 
 echo "Unzipping"
 unzip -o /tmp/cart.zip &>>/tmp/cart.log && mv cart-main cart && cd cart &>>/tmp/cart.log
-status_check()
+status_check();
 
 echo "installing NodeJS dependencies"
 npm install &>>/tmp/cart.log
-status_check()
+status_check();
 
 echo "configuring cart systemd service "
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
-status_check()
+status_check();
 
 echo "starting service"
 systemctl start cart && systemctl enable cart &>>/tmp/cart.log
-status_check()
+status_check();
