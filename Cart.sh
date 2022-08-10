bf19acd769395113864b0ff66a3c5cc4335fd235
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-10 15:19:11 -0500

    updating ./RoboShop-Automation

--- Cart.sh
+++ Cart.sh
@@ -1,73 +1,45 @@
+status_check()
+{
+  if [ $? == 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+  fi
+}
 
 echo "setting nodejs repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check()
 
 echo "Installing NodeJS"
 yum install nodejs -y &>>/tmp/cart.log
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check()
 
 echo "Adding user"
 useradd roboshop &>>/tmp/cart.log
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check()
 
 echo "Downloading application content"
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" && cd /home/roboshop &>>/tmp/cart.log
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check()
 
 echo "Deleting the old application content"
 rm -rf cart &>>/tmp/cart.log
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check()
 
 echo "Unzipping"
 unzip -o /tmp/cart.zip &>>/tmp/cart.log && mv cart-main cart && cd cart &>>/tmp/cart.log
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check()
 
 echo "installing NodeJS dependencies"
 npm install &>>/tmp/cart.log
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check()
 
 echo "configuring cart systemd service "
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check()
 
 echo "starting service"
 systemctl start cart && systemctl enable cart &>>/tmp/cart.log
-if [ $? == 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+status_check()
