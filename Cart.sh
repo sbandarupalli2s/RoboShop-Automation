771ab2ee59a2b06d38d269374061465d18e51bcb
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-10 13:52:06 -0500

    updating ./RoboShop-Automation

--- Cart.sh
+++ Cart.sh
@@ -9,38 +9,70 @@ fi
 
 echo "Installing NodeJS"
 yum install nodejs -y &>>/tmp/cart.log
-echo $?
+if [ $? == 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo "Adding user"
 useradd roboshop &>>/tmp/cart.log
-echo $?
+if [ $? == 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo "Downloading application content"
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
-echo $?
+if [ $? == 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 cd /home/roboshop &>>/tmp/cart.log
 echo "Deleting the old application content"
 rm -rf cart &>>/tmp/cart.log
-echo $?
+if [ $? == 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo "Unzipping"
 unzip -o /tmp/cart.zip &>>/tmp/cart.log
-echo $?
+if [ $? == 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 mv cart-main cart &>>/tmp/cart.log
 cd cart
 
 echo "installing NodeJS dependencies"
 npm install &>>/tmp/cart.log
-echo $?
+if [ $? == 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo "configuring cart systemd service "
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
-echo $?
+if [ $? == 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 
 echo "starting service"
 systemctl start cart &>>/tmp/cart.log
-echo $?
+if [ $? == 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mSUCCESS\e[0m"
+fi
 systemctl enable cart &>>/tmp/cart.log
