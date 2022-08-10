c6d7a72c951d34b57a8661ea0d950de73db8352b
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-10 00:06:53 -0500

    updating ./RoboShop-Automation

--- MySQL.sh
+++ MySQL.sh
@@ -1,24 +1,24 @@
 
 set -e
 
- curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>/tmp/mysql.log
 
- yum install mysql-community-server -y
+yum install mysql-community-server -y &>>/tmp/mysql.log
 
- systemctl enable mysqld
- systemctl start mysqld
+systemctl enable mysqld &>>/tmp/mysql.log
+systemctl start mysqld &>>/tmp/mysql.log
 
- grep temp /var/log/mysqld.log
+grep temp /var/log/mysqld.log &>>/tmp/mysql.log
 
- mysql_secure_installation
+mysql_secure_installation &>>/tmp/mysql.log
 
- mysql -uroot -pRoboShop@1
+mysql -uroot -pRoboShop@1 &>>/tmp/mysql.log
 
- uninstall plugin validate_password;
+uninstall plugin validate_password; &>>/tmp/mysql.log
 
- curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>/tmp/mysql.log
 
- cd /tmp
- unzip mysql.zip
- cd mysql-main
- mysql -u root -pRoboShop@1 <shipping.sql
+cd /tmp &>>/tmp/mysql.log
+unzip mysql.zip &>>/tmp/mysql.log
+cd mysql-main &>>/tmp/mysql.log
+mysql -u root -pRoboShop@1 <shipping.sql &>>/tmp/mysql.log
