2b00b6821a0f02eb1b67972767af3aa5910f7da9
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-10 13:58:26 -0500

    updating ./RoboShop-Automation

--- Cart.sh
+++ Cart.sh
@@ -4,7 +4,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
 if [ $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mSUCCESS\e[0m"
+  echo -e "\e[31mFAILURE\e[0m"
 fi
 
 echo "Installing NodeJS"
@@ -12,7 +12,7 @@ yum install nodejs -y &>>/tmp/cart.log
 if [ $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mSUCCESS\e[0m"
+  echo -e "\e[31mFAILURE\e[0m"
 fi
 
 echo "Adding user"
@@ -20,7 +20,7 @@ useradd roboshop &>>/tmp/cart.log
 if [ $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mSUCCESS\e[0m"
+  echo -e "\e[31mFAILURE\e[0m"
 fi
 
 echo "Downloading application content"
@@ -28,7 +28,7 @@ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/arc
 if [ $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mSUCCESS\e[0m"
+  echo -e "\e[31mFAILURE\e[0m"
 fi
 
 cd /home/roboshop &>>/tmp/cart.log
@@ -37,7 +37,7 @@ rm -rf cart &>>/tmp/cart.log
 if [ $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mSUCCESS\e[0m"
+  echo -e "\e[31mFAILURE\e[0m"
 fi
 
 echo "Unzipping"
@@ -45,7 +45,7 @@ unzip -o /tmp/cart.zip &>>/tmp/cart.log
 if [ $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mSUCCESS\e[0m"
+  echo -e "\e[31mFAILURE\e[0m"
 fi
 
 mv cart-main cart &>>/tmp/cart.log
@@ -56,7 +56,7 @@ npm install &>>/tmp/cart.log
 if [ $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mSUCCESS\e[0m"
+  echo -e "\e[31mFAILURE\e[0m"
 fi
 
 echo "configuring cart systemd service "
@@ -65,14 +65,13 @@ systemctl daemon-reload &>>/tmp/cart.log
 if [ $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mSUCCESS\e[0m"
+  echo -e "\e[31mFAILURE\e[0m"
 fi
 
 echo "starting service"
-systemctl start cart &>>/tmp/cart.log
+systemctl start cart && systemctl enable cart &>>/tmp/cart.log
 if [ $? == 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mSUCCESS\e[0m"
+  echo -e "\e[31mFAILURE\e[0m"
 fi
-systemctl enable cart &>>/tmp/cart.log
