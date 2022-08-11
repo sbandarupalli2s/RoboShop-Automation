8f0270c35f2eb47471a543d48ca43eb955c0c0fe
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-10 19:21:45 -0500

    updating ./RoboShop-Automation

--- Cart.sh
+++ Cart.sh
@@ -1,4 +1,12 @@
-source Common.sh
+status_check()
+{
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 1
+  fi
+}
 
 echo "setting nodejs repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
--- Common.sh
+++ Common.sh
@@ -1,5 +1,4 @@
-status_check()
-{
+status_check() {
   if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
