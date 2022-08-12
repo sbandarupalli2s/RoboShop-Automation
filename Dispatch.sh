651b7178fddac4263217166342dbf0aa3053741f
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-12 14:52:55 -0500

    updating ./RoboShop-Automation

--- Dispatch.sh
+++ Dispatch.sh
@@ -7,7 +7,6 @@ echo "Downloading the code for dispatch"
 curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip &>>/tmp/dispatch.log
 status_check
 
-cd /home/roboshop
 echo "deleting the old content"
 rm -rf catalogue
 status_check
@@ -17,6 +16,7 @@ echo "unzipping the file"
 unzip /tmp/dispatch.zip &>>/tmp/catalogue.log
 status_check
 
+cd /home/roboshop
 mv dispatch-main dispatch && cd dispatch
 #go mod init dispatch
 #go get
