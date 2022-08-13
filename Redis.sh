2d1212fdc9e3dacc7d810da98bb39a1bec55bf70
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-13 16:08:40 -0700

    updating ./RoboShop-Automation

--- Redis.sh
+++ Redis.sh
@@ -1,8 +1,13 @@
-set -e
+echo "This is redis server"
 
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-yum install redis-6.2.7 -y
+echo "Downloading the source code..."
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>/tmp/redis.log
+status_check
 
+echo "Installing the redis... "
+yum install redis-6.2.7 -y &>>/tmp/redis.log
+status_check
 
-systemctl enable redis
-systemctl start redis
+echo "starting redis..."
+systemctl enable redis && systemctl start redis &>>/tmp/redis.log
+status_check
