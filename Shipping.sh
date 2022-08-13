8b9cf0ffce4c43baa35b46956888c4b0c1d5f521
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-13 16:33:08 -0700

    updating ./RoboShop-Automation

--- Shipping.sh
+++ Shipping.sh
@@ -1,18 +1,32 @@
- set -e
+source Common.sh
 
- yum install maven -y
+echo installing the maven
+yum install maven -y &>>/tmp/shipping.log
+status_check
 
- useradd roboshop
+AddUsr
 
- cd /home/roboshop
- curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
- unzip /tmp/shipping.zip
- mv shipping-main shipping
- cd shipping
- mvn clean package
- mv target/shipping-1.0.jar shipping.jar
+cd /home/roboshop
+echo downloading the shipping code.
+curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip" &>>/tmp/shipping.log
+status_check
 
- mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
- systemctl daemon-reload
- systemctl start shipping 
- systemctl enable shipping
+echo unzipping..
+unzip -o /tmp/shipping.zip &>>/tmp/shipping.log
+status_check
+
+mv shipping-main shipping
+cd shipping
+
+echo cleaning the maven packages.
+mvn clean package &>>/tmp/shipping.log
+status_check
+
+mv target/shipping-1.0.jar shipping.jar
+mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
+echo "daemon-reloading..."
+systemctl daemon-reload &>>/tmp/shipping.log
+status_check
+
+echo "starting the shipping service."
+systemctl start shipping && systemctl enable shipping &>>/tmp/shipping.log
