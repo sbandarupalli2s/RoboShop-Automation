2676abb22f7d8bd4225b0d136cd12c2bdd4dc61f
sbandarupalli2s <seetharamaiah224@gmail.com>
2022-08-15 17:08:28 -0700

    updating ./RoboShop-Automation

--- Catalouge.sh
+++ Catalouge.sh
@@ -1,33 +1,4 @@
 COMPONENT=catalogue
 
 source Common.sh
-
 NodeJS_install
-AddUsr
-
-#sudo su roboshop
-
-echo "Downloading the code for catalogue"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/catalogue.log
-status_check
-
-cd /home/roboshop
-rm -rf catalogue
-echo "unzipping the file"
-unzip -o /tmp/catalogue.zip &>>/tmp/catalogue.log
-status_check
-
-cd /home/roboshop
-mv catalogue-main catalogue && cd /home/roboshop/catalogue
-
-echo "installing dependencies "
-npm install &>>/tmp/catalogue.log
-status_check
-
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-
-echo "starting catalogue "
-systemctl daemon-reload &>>/tmp/catalogue.log
-systemctl start catalogue && systemctl enable catalogue &>>/tmp/catalogue.log
-status_check
-
